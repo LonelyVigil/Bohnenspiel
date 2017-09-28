2ce8a10fa42d069ed6abf025ba6ba52de3d84002
JasZal <jasminzalonis@web.de>
2017-09-28 22:34:51 +0200

    gekürzte SuperHeuristik

--- BohnenspielVorlage/src/ai/Zauberbohne.java
+++ BohnenspielVorlage/src/ai/Zauberbohne.java
@@ -9,7 +9,7 @@ public class Zauberbohne {
 
   int random = 1;
   int gespeicherterZug = -1;
-  int gewuenschteTiefe = 6;
+  int gewuenschteTiefe = 8;
   Gameboard copy = new Gameboard();
   Gameboard original = new Gameboard();
 
@@ -141,254 +141,225 @@ public class Zauberbohne {
   }
 
 
-  
-   /**
+
+  /**
    * Heuristic for strategy
    *
    * @return
    */
-   private int bewerten3(int move) {
-  
-   ///DIE AKTUELLE HEURISTIK GEWINNT IMMER MIT 30:40
-   //wäre also zum bestehen des projektes geeignet
-   //Allerdings nur mit Suchtiefe bis 8
-   int wert = 0;
-   if (playerOne) {
-   wert = original.getTreasuryOne() * 4;
-   wert -= original.getTreasuryTwo() * 2;
-  
-   wert += original.getHolesWithLotOfBeans(true);
-   if (original.moveLeft(1, false)) {
-   wert += original.getSumOwnRow(false);
-   }
-   } else {
-   wert = original.getTreasuryTwo() * 4;
-   wert += original.getHolesWithLotOfBeans(false);
-   wert -= original.getTreasuryOne() * 2;
-   if (original.moveLeft(1, true)) {
-   wert += original.getSumOwnRow(true);
-   }
-   }
-  
-   switch (original.getState()[move - 1]) {
-   case 1:
-   wert += 1;
-   break;
-   case 3:
-   wert += 3;
-   break;
-   case 5:
-   wert += 5;
-   break;
-  
-   }
-  
-   return wert;
-   }
-
-//Die hier ist schlechter!
   private int bewerten2(int move) {
+
     int wert = 0;
     if (playerOne) {
-      wert = original.getTreasuryOne();
-      wert -= original.getTreasuryTwo();
+      wert = original.getTreasuryOne() * 4;
+      wert -= original.getTreasuryTwo() * 2;
+
+      wert += original.getHolesWithLotOfBeans(true);
+      if (original.moveLeft(1, false)) {
+        wert += original.getSumOwnRow(false);
+      }
     } else {
-      wert = original.getTreasuryTwo();
-      wert -= original.getTreasuryOne();
+      wert = original.getTreasuryTwo() * 4;
+      wert += original.getHolesWithLotOfBeans(false);
+      wert -= original.getTreasuryOne() * 2;
+      if (original.moveLeft(1, true)) {
+        wert += original.getSumOwnRow(true);
+      }
+    }
+
+    switch (original.getState()[move - 1]) {
+      case 1:
+        wert += 1;
+        break;
+      case 3:
+        wert += 3;
+        break;
+      case 5:
+        wert += 5;
+        break;
+
     }
     return wert;
   }
-  
+
+
   private int bewerten(int move) {
-     // Gameboard future = new Gameboard();
-      //future = copy.copy();
-     // future.doMove(move, false);
-      //start value
-      int value = 0;
-      if(playerOne) {
-	  //do I win beans? does the opponent win beans?
-	 // value += 2*future.getTreasuryOne()-copy.getTreasuryOne();
-	//  value -= 4*future.getTreasuryTwo()-copy.getTreasuryTwo();
-	  
-	  //attackable own 1, 3 and 5 are problematic
-	  for(int i=0; i<6; i++) {
-	      if(original.getState()[i]==1 || original.getState()[i]==3 || original.getState()[i]==5) {
-		  for(int j=6; j<=11; j++) {
-		      if(original.getState()[j]==j-i || original.getState()[j]==12+j-i) {
-			  value-=original.getState()[i]*2;
-		      }
-		  }
-	      }
-	  }
-	  
-	  //attackable 1, 3 and 5 on opponent's side are good
-	  for(int i=6; i<=11; i++) {
-	      if(original.getState()[i]==1 || original.getState()[i]==3 || original.getState()[i]==5) {
-		  for(int j=0; j<6; j++) {
-		      if(original.getState()[j]==j-i || original.getState()[j]==12+j-i) {
-			  value+=original.getState()[i]*2;
-		      }
-		  }
-	      }
-	  }
-	  
-	  //too many own empty fields are bad
-	  int count =0;
-	  for(int i=0; i<6; i++) {
-	      if(original.getState()[i]==0) {
-		  count++;
-	      }
-	  }
-	  switch(count) {
-	  case 0: value+=2; break;
-	  case 1: value++; break;
-	  case 2: break;
-	  case 3: value--; break;
-	  case 4: value-=3; break;
-	  case 5: value-=5; break;
-	  case 6: value-=7; break;
-	  }
-	  
-	  //many empty fields on opponent's side are good
-	  count =0;
-	  for(int i=6; i<=11; i++) {
-	      if(original.getState()[i]==0) {
-		  count++;
-	      }
-	  }
-	  switch(count) {
-	  case 0: value-=2; break;
-	  case 1: value--; break;
-	  case 2: break;
-	  case 3: value++; break;
-	  case 4: value+=3; break;
-	  case 5: value+=5; break;
-	  case 6: value+=7; break;
-	  }
-	  
-	  //putting more beans on own side is good, otherwise bad
-	  int differenceOwnSide = original.getSumOwnRow(playerOne)-original.getSumOwnRow(playerOne);
-	  int differenceOtherSide = original.getSumOwnRow(!playerOne)-original.getSumOwnRow(!playerOne);
-	  if(differenceOwnSide > differenceOtherSide) {
-	      value+=2;
-	  }
-	  else {
-	      value-=2;
-	  }
-	  
-	  //owning fields with a lot beans is good, it's bad, if the opponent has some
-	  for(int i=0; i<6; i++) {
-	      if(original.getState()[i]>=6) {
-		  value++;
-	      }
-	  }
-	  for(int i=6; i<=11; i++) {
-	      if(original.getState()[i]>=6) {
-		  value--;
-	      }
-	  }
-	  
-	  //using high numbers if one's low on opportunities is good
-	  if(original.getState()[move-1]>=10 && move-1<6) {
-	      value+=3;
-	  }
-	  if(original.getState()[move-1]>=10 && move-1>=6) {
-	      value-=3;
-	  }
+    int value = 0;
+    if (playerOne) {
+
+      value = attackablefields(value, 2);
+      value = checkEmptyField(value, 2);
+      value = owningFields(value, 2);
+
+      // putting more beans on own side is good, otherwise bad
+      int differenceOwnSide = original.getSumOwnRow(playerOne) - original.getSumOwnRow(playerOne);
+      int differenceOtherSide =
+          original.getSumOwnRow(!playerOne) - original.getSumOwnRow(!playerOne);
+      if (differenceOwnSide > differenceOtherSide) {
+        value += 2;
+      } else {
+        value -= 2;
       }
-      
-      else {
-	//do I win beans? does the opponent win beans?
-	//  value += 2*future.getTreasuryTwo()-copy.getTreasuryTwo();
-	//  value -= 4*future.getTreasuryOne()-copy.getTreasuryOne();
-	  
-	  //attackable own 1, 3 and 5 are problematic
-	  for(int i=6; i<=11; i++) {
-	      if(original.getState()[i]==1 || original.getState()[i]==3 || original.getState()[i]==5) {
-		  for(int j=0; j<6; j++) {
-		      if(original.getState()[j]==j-i || original.getState()[j]==12+j-i) {
-			  value-=original.getState()[i]*2;
-		      }
-		  }
-	      }
-	  }
-	 
-	  //attackable 1, 3 and 5 on opponent's side are good
-	  for(int i=0; i<6; i++) {
-	      if(original.getState()[i]==1 || original.getState()[i]==3 || original.getState()[i]==5) {
-		  for(int j=6; j<=11; j++) {
-		      if(original.getState()[j]==j-i || original.getState()[j]==12+j-i) {
-			  value+=original.getState()[i]*2;
-		      }
-		  }
-	      }
-	  }
-	  
-	  //too many own empty fields are bad
-	  int count =0;
-	  for(int i=6; i<=11; i++) {
-	      if(original.getState()[i]==0) {
-		  count++;
-	      }
-	  }
-	  switch(count) {
-	  case 0: value+=2; break;
-	  case 1: value++; break;
-	  case 2: break;
-	  case 3: value--; break;
-	  case 4: value-=2; break;
-	  case 5: value-=3; break;
-	  case 6: value-=4; break;
-	  }
-	  
-	  //many empty fields on opponent's side are good
-	  count =0;
-	  for(int i=0; i<6; i++) {
-	      if(original.getState()[i]==0) {
-		  count++;
-	      }
-	  }
-	  switch(count) {
-	  case 0: value-=2; break;
-	  case 1: value--; break;
-	  case 2: break;
-	  case 3: value++; break;
-	  case 4: value+=2; break;
-	  case 5: value+=3; break;
-	  case 6: value+=4; break;
-	  }
-	  
-	  //putting more beans on own side is good, otherwise bad
-	  int differenceOwnSide = original.getSumOwnRow(!playerOne)-original.getSumOwnRow(!playerOne);
-	  int differenceOtherSide = original.getSumOwnRow(playerOne)-original.getSumOwnRow(playerOne);
-	  if(differenceOwnSide > differenceOtherSide) {
-	      value+=2;
-	  }
-	  else {
-	      value-=2;
-	  }
-	  
-	  //owning fields with a lot beans is good, it's bad, if the opponent has some
-	  for(int i=6; i<=11; i++) {
-	      if(original.getState()[i]>=6) {
-		  value++;
-	      }
-	  }
-	  for(int i=0; i<6; i++) {
-	      if(original.getState()[i]>=6) {
-		  value--;
-	      }
-	  }
-	  
-	//using high numbers if one's low on opportunities is good
-	  if(original.getState()[move-1]>=10 && move-1>=6) {
-	      value+=3;
-	  }
-	  if(original.getState()[move-1]>=10 && move-1<6) {
-	      value-=3;
-	  }
+
+      // using high numbers if one's low on opportunities is good
+      if (original.getState()[move - 1] >= 10 && move - 1 < 6) {
+        value += 3;
       }
-      
-      return value;
+      if (original.getState()[move - 1] >= 10 && move - 1 >= 6) {
+        value -= 3;
+      }
+    }
+
+    else {
+      value = attackablefields(value, 1);
+      value = checkEmptyField(value, 1);
+      value = owningFields(value, 1);
+
+      // putting more beans on own side is good, otherwise bad
+      int differenceOwnSide = original.getSumOwnRow(!playerOne) - original.getSumOwnRow(!playerOne);
+      int differenceOtherSide = original.getSumOwnRow(playerOne) - original.getSumOwnRow(playerOne);
+      if (differenceOwnSide > differenceOtherSide) {
+        value += 2;
+      } else {
+        value -= 2;
+      }
+
+      // using high numbers if one's low on opportunities is good
+      if (original.getState()[move - 1] >= 10 && move - 1 >= 6) {
+        value += 3;
+      }
+      if (original.getState()[move - 1] >= 10 && move - 1 < 6) {
+        value -= 3;
+      }
+    }
+
+    return value;
+  }
+
+
+
+  /**
+   * @param value
+   * @param k
+   * @return
+   */
+  private int owningFields(int value, int k) {
+    // owning fields with a lot beans is good, it's bad, if the opponent has some
+    for (int i = 0; i < 6; i++) {
+      if (original.getState()[i] >= 6) {
+        value += (int) (Math.pow(-1, k) * 1);
+      }
+    }
+    for (int i = 6; i <= 11; i++) {
+      if (original.getState()[i] >= 6) {
+        value -= (int) (Math.pow(-1, k) * 1);
+      }
+    }
+    return value;
+  }
+
+
+
+  /**
+   * @param value
+   * @param k
+   * @return
+   */
+  private int checkEmptyField(int value, int k) {
+    // too many own empty fields are bad
+    int count = 0;
+    for (int i = 0; i < 6; i++) {
+      if (original.getState()[i] == 0) {
+        count++;
+      }
+    }
+    switch (count) {
+      case 0:
+        value += (int) (Math.pow(-1, k) * 2);
+        break;
+      case 1:
+        value += (int) (Math.pow(-1, k) * 1);
+        break;
+      case 2:
+        break;
+      case 3:
+        value += (int) (Math.pow(-1, k) * -1);
+        break;
+      case 4:
+        value -= (int) (Math.pow(-1, k) * -2);
+        break;
+      case 5:
+        value -= (int) (Math.pow(-1, k) * -3);
+        break;
+      case 6:
+        value -= (int) (Math.pow(-1, k) * -4);
+        break;
+    }
+
+    // many empty fields on opponent's side are good
+    count = 0;
+    for (int i = 6; i <= 11; i++) {
+      if (original.getState()[i] == 0) {
+        count++;
+      }
+    }
+    switch (count) {
+      case 0:
+        value -= (int) (Math.pow(-1, k) * 2);
+        break;
+      case 1:
+        value -= (int) (Math.pow(-1, k) * 1);
+        break;
+      case 2:
+        break;
+      case 3:
+        value += (int) (Math.pow(-1, k) * 1);
+        break;
+      case 4:
+        value += (int) (Math.pow(-1, k) * 2);
+        break;
+      case 5:
+        value += (int) (Math.pow(-1, k) * 3);
+        break;
+      case 6:
+        value += (int) (Math.pow(-1, k) * 4);
+        break;
+    }
+    return value;
+  }
+
+
+
+  /**
+   * @param value
+   * @param k
+   * @return
+   */
+  private int attackablefields(int value, int k) {
+    // attackable own 1, 3 and 5 are problematic
+    for (int i = 0; i < 6; i++) {
+      if (original.getState()[i] == 1 || original.getState()[i] == 3
+          || original.getState()[i] == 5) {
+        for (int j = 6; j <= 11; j++) {
+          if (original.getState()[j] == j - i || original.getState()[j] == 12 + j - i) {
+            value -= (int) (Math.pow(-1, k) * original.getState()[i] * 2);
+          }
+        }
+      }
+    }
+
+    // attackable 1, 3 and 5 on opponent's side are good
+    for (int i = 6; i <= 11; i++) {
+      if (original.getState()[i] == 1 || original.getState()[i] == 3
+          || original.getState()[i] == 5) {
+        for (int j = 0; j < 6; j++) {
+          if (original.getState()[j] == j - i || original.getState()[j] == 12 + j - i) {
+            value += (int) (Math.pow(-1, k) * original.getState()[i] * 2);
+          }
+        }
+      }
+    }
+
+    return value;
   }
 }
